3416823ce34174000910c6cd6c9239034d8ea291
Miguel Coba <miguel.coba@gmail.com>
2015-06-11 06:32:55 +0000

    First version of Ember.js Introduction example app

--- js/app.js
+++ js/app.js
@@ -1,11 +1,53 @@
 App = Ember.Application.create();
 
+App.attendants = [
+	{ id: 1, firstName: 'Tom', lastName: 'Dale', email: 'tom@emberjs.com' },
+	{ id: 2, firstName: 'Yehuda', lastName: 'katz', email: 'wycats@emberjs.com' }
+];
+
 App.Router.map(function() {
   // put your routes here
+	this.route('register');
+	this.route('registered');
+	this.route('attendants', function() {
+		this.route('about', { path: '/:attendant_id/about'});
+	});
 });
 
-App.IndexRoute = Ember.Route.extend({
+App.RegisterRoute = Ember.Route.extend({
+	model: function() {
+		return { id: App.attendants.length + 1, firstName: "", lastName: "", email: ""}
+	}
+});
+
+App.RegisterController = Ember.Controller.extend({
+	isInvalid: true,
+
+	validForm: function() {
+		var isValid = this.get('model.firstName') != "" && this.get('model.lastName') != "" && this.get('model.email') != "";
+		this.set('isInvalid', !isValid);
+	}.observes('model.firstName', 'model.lastName', 'model.email'),
+
+	actions: {
+		register: function() {
+			var newAttendant = Ember.copy(this.get('model'));
+			App.attendants.push(newAttendant);
+			this.transitionToRoute('index');
+		}
+	}
+});
+
+App.AttendantsRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return App.attendants;
+  }
+});
+
+App.AttendantsController = Ember.ArrayController.extend({
+});
+
+App.AttendantsAboutRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.attendants.findBy('id', params.attendant_id);
   }
 });
